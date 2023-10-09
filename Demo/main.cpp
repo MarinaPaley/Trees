68436b21c0a0cec32f8c56396b0cb13546984b7a
ludmilanv <ludmilanv@mail.ru>
2023-10-09 16:53:02 +0300

    Начало

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -1,4 +1,11 @@
+#include <iostream>
+#include "../Trees/Node.h"
+
+using namespace rut::cip;
 int main()
 {
+	Node<int> node{ 1 };
+	Node<int> other{ 2 };
+	std::cout << (node != other) << std::endl;
 	return 0;
 }
--- Trees/Node.h
+++ Trees/Node.h
@@ -1,6 +1,13 @@
 #pragma once
+#include <ostream>
+#include <sstream>
 namespace rut::cip 
 {
+	template <typename T> struct Node;
+
+	template <typename T>
+	std::ostream& operator<<(std::ostream& out, const Node<T>& node);
+
 	template <typename T>
 	struct Node
 	{
@@ -15,7 +22,39 @@ namespace rut::cip
 		Node& operator=(const Node& other) = delete;
 		Node& operator=(Node&& other) noexcept = default;
 		~Node() = default;
-		friend auto operator<=>(const Node& lha, const Node& rha);
-		friend bool operator==(const Node& lha, const Node& rha);
+		friend auto operator<=>(const Node& lha, const Node& rha)
+		{
+			if (std::less<T>()(lha.data, rha.data))
+			{
+				return 1;
+			}
+
+			if (std::greater<T>()(lha.data, rha.data))
+			{
+				return -1;
+			}
+
+			return 0;
+		}
+
+		friend bool operator==(const Node& lha, const Node& rha)
+		{
+			return (operator<=>(lha, rha) == 0);
+		}
 	};
+	template<typename T>
+	inline Node<T>::Node(const T& value)
+		: data{ value }, parent{nullptr}, left{nullptr}, right{nullptr}
+	{
+	}
+
+	template<typename T>
+	std::ostream& operator<<(std::ostream& out, const Node<T>& node)
+	{
+		std::stringstream buffer{};
+		buffer << node.data;
+		out << buffer.str();
+		return out;
+	}
+
 }
